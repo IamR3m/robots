33d0777ea0e0ca76075741212476ec57a223bd25
IamR3m <klubberzone@gmail.com>
2020-01-15 10:11:43 +0300

    Made GameControllerTest to be working

--- src/test/java/com/alexkasko/robots/controller/GameControllerTest.java
+++ src/test/java/com/alexkasko/robots/controller/GameControllerTest.java
@@ -1,13 +1,20 @@
 package com.alexkasko.robots.controller;
 
+import com.alexkasko.robots.config.ThreadConfig;
+import com.alexkasko.robots.config.WebAppInitializer;
+import com.alexkasko.robots.config.WebConfig;
 import com.alexkasko.robots.entity.MessageLog;
 import com.alexkasko.robots.entity.Task;
+import com.alexkasko.robots.service.ActivityTracker;
+import com.alexkasko.robots.service.ActivityTrackerRunner;
+import com.alexkasko.robots.service.MessageLogService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -15,21 +22,32 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-/*
-* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-* !!!!!!!!!!!!!    WARNING    !!!!!!!!!!!!!!
-* Test don't work. You shouldn't run it!!!!!
-* */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = WebConfig.class)
 @WebAppConfiguration
 public class GameControllerTest {
 
     @Autowired
-    GameController gameController;
+    ActivityTracker activityTracker;
+
+    @Autowired
+    ActivityTrackerRunner activityTrackerRunner;
+
+    @Autowired
+    MessageLogService messageLogService;
+
+    @Autowired
+    ThreadConfig threadConfig;
+
+    private GameController gameController;
 
     @Before
     public void setUp() throws Exception {
+        this.gameController = new GameController(activityTracker,
+                activityTrackerRunner,
+                messageLogService,
+                threadConfig.threadPoolTaskExecutor());
     }
 
     @Test
